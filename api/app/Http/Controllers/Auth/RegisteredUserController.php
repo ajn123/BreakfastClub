a5fffb1ff966a6c7c1de811f06d897edcc24a710
ajn123 <alexander.norton@frontgatetickets.com>
2024-12-07 18:14:34 -0500

    introductory email and tested

--- api/app/Http/Controllers/Auth/RegisteredUserController.php
+++ api/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -2,16 +2,19 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Models\User;
-use Inertia\Inertia;
-use Inertia\Response;
-use Illuminate\Http\Request;
-use Illuminate\Validation\Rules;
 use App\Http\Controllers\Controller;
+use App\Mail\WelcomeMail;
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Validation\Rules;
+use Inertia\Inertia;
+use Inertia\Response;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Auth\Events\Registered;
 
 class RegisteredUserController extends Controller
 {
@@ -25,14 +28,12 @@ class RegisteredUserController extends Controller
 
     /**
      * Handle an incoming registration request.
-     *
-     * @throws \Illuminate\Validation\ValidationException
      */
     public function store(Request $request): RedirectResponse
     {
         $request->validate([
             'name' => 'required|string|max:255',
-            'email' => 'required|string|lowercase|email|max:255|unique:'.User::class,
+            'email' => 'required|string|email|max:255|unique:users',
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
@@ -44,8 +45,11 @@ class RegisteredUserController extends Controller
 
         event(new Registered($user));
 
+        // Send welcome email
+        Mail::to($user->email)->send(new WelcomeMail($user));
+
         Auth::login($user);
 
-        return redirect(route('dashboard', absolute: false));
+        return redirect()->route('dashboard');
     }
 }
