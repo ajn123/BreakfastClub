acffcd154c903726a692b922cc351413d2ad4f4d
ajn123 <ajn123@vt.edu>
2024-12-22 10:20:46 -0500

    comment out login tests

--- tests/Browser/LoginTest.php
+++ tests/Browser/LoginTest.php
@@ -2,10 +2,10 @@
 
 use Laravel\Dusk\Browser;
 
-test('example', function () {
-    $this->browse(function (Browser $browser) {
-        $browser->visit('/login')
-            ->waitFor('.text-6xl', 10)
-            ->assertSee('Your Expected Text Here');
-    });
-});
+// test('example', function () {
+//     $this->browse(function (Browser $browser) {
+//         $browser->visit('/login')
+//             ->waitFor('.text-6xl', 10)
+//             ->assertSee('Your Expected Text Here');
+//     });
+// });
