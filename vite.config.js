908286362db0ccd4129f29fa7c11ee9ed070bd45
ajn123 <ajn123@vt.edu>
2025-01-11 18:47:30 -0500

    added middleware

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,6 @@ export default defineConfig({
     server: {
         host: '0.0.0.0',
         port: 5173,
-        https: true,
         hmr: {
             host: 'localhost',
             port: 5173,
