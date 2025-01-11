2f54981e4d8743fb93bc2967e44d4ed784ffba6e
ajn123 <ajn123@vt.edu>
2025-01-11 16:34:16 -0500

    added cert configuration

--- resources/js/bootstrap.ts
+++ resources/js/bootstrap.ts
@@ -3,4 +3,5 @@ window.axios = axios;
 
 window.axios.defaults.withCredentials = true;
 window.axios.defaults.withXSRFToken = true;
+window.axios.defaults.baseURL = window.location.protocol === 'https:' ? 'https://' + window.location.hostname : 'http://' + window.location.hostname;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
